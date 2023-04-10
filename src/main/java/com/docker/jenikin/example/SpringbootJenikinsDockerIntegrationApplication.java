35b80dbce69ac3bf84d765dc72257a76d3daa91b
jenikinsdocker <jenikinsdocker@gmail.com>
2023-04-10 15:41:31 +0530

    dsaddsa

--- src/main/java/com/docker/jenikin/example/SpringbootJenikinsDockerIntegrationApplication.java
+++ src/main/java/com/docker/jenikin/example/SpringbootJenikinsDockerIntegrationApplication.java
@@ -8,6 +8,8 @@ public class SpringbootJenikinsDockerIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenikinsDockerIntegrationApplication.class, args);
+		
+		System.out.println("hello....");
 	}
 
 }
